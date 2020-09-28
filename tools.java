ab4e5abf25dbfca7443260ee652cd976c3c30b13
Albert <szulcalbert@gmail.com>
2020-09-28 19:02:58 +0200

    init - independent tools for projects

---
+++ tools.java
@@ -0,0 +1,20 @@
+package editor;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class tools {
+
+    public static void setMargin(JComponent aComponent, int aTop,
+                                 int aRight, int aBottom, int aLeft) {
+        Border border = aComponent.getBorder();
+
+        Border marginBorder = new EmptyBorder(new Insets(aTop, aLeft,
+                aBottom, aRight));
+        aComponent.setBorder(border == null ? marginBorder
+                : new CompoundBorder(marginBorder, border));
+    }
+}
